ad826bf7c7d2541f6cc919927703760a1d15bfd6
markthumes <markthumes@gmail.com>
2022-04-29 12:01:44 -0500

    Added shader files and texture

---
+++ demo/vshader.glsl
@@ -0,0 +1,21 @@
+#ifdef GL_ES
+// Set default precision to medium
+precision mediump int;
+precision mediump float;
+#endif
+
+uniform mat4 mvp_matrix;
+
+attribute vec4 a_position;
+attribute vec3 a_normal;
+attribute vec3 a_color;
+attribute vec2 a_texcoord;
+
+varying vec2 v_texcoord;
+varying vec3 v_color;
+
+void main(){
+    gl_Position = mvp_matrix * a_position;
+    v_texcoord = a_texcoord;
+    v_color = a_color;
+}
